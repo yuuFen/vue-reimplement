6b63803868e1411d1ed4158026e7df23757448eb
Yu <me@yuufen.com>
2020-03-08 00:47:46 +0800

    依赖收集与追踪

--- vue.js
+++ vue.js
@@ -12,6 +12,10 @@ class Vue {
       // 不然 set 多余的键时会给 data 添加属性
       this.proxyData(key)
     })
+
+    // 测试
+    new Watcher(this, 'foo')
+    new Watcher(this, 'bar.doo')
   }
 
   observe(value) {
@@ -31,15 +35,22 @@ class Vue {
     // 递归，处理对象
     this.observe(val)
 
+    //
+    const dep = new Dep()
+
     // 会在 obj 上添加新属性，或者修改现有属性
     // 所以是个引用变量
     Object.defineProperty(obj, key, {
       get() {
+        // 若存在 targetWatcher（watcher的构造阶段），则添加
+        Dep.targetWatcher && dep.addWatcher(Dep.targetWatcher)
         return val
       },
       set(newVal) {
         if (newVal !== val) {
           val = newVal
+          // 通知更新
+          dep.notify()
         }
       },
     })
@@ -56,5 +67,36 @@ class Vue {
         this.$data[key] = newVal
       },
     })
+  }
+}
+
+// 创建 Dep: 管理所有的 Watcher
+class Dep {
+  constructor() {
+    this.watchers = []
+  }
+  addWatcher(watcher) {
+    this.watchers.push(watcher)
+  }
+  notify() {
+    this.watchers.forEach((watcher) => {
+      watcher.update()
+    })
+  }
+}
+
+// 创建 Watcher：与视图中的变量对应(出现一次就创建一个 Watcher 实例)
+class Watcher {
+  constructor(vm, key) {
+    this.vm = vm
+    this.key = key
+
+    Dep.targetWatcher = this
+    this.vm[this.key] // key解析出来之后为字符串， 但是若如 'bar.doo'，需要额外操作
+    Dep.targetWatcher = null
+  }
+  update() {
+    console.log(this.key, '更新——来自 watcher')
+    // do something
   }
 }
